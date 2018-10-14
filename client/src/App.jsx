64ff0fedb84de14c38c7b7b4565a6ddc171cf07a
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-10-13 17:27:03 -0700

    Add sample code to React component and HTML

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default App;
