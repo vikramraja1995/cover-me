c586833f8ac85e84f392703290a246419d0de471
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-10-14 18:57:27 -0700

    Update form to use Material theme

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -10,7 +10,8 @@ class Form extends React.Component {
       role: '',
       industry: '',
       frontEndFramework: '',
-      database: ''
+      database: '',
+      missing: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -20,16 +21,19 @@ class Form extends React.Component {
     return roles.map((role, i) => {
       return (
         <div key={`role-${i}`} className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="role"
-            id={`role-${i}`}
-            value={role}
-            onClick={() => this.setState({ role })}
-          />
           <label className="form-check-label" htmlFor={`role-${i}`}>
             {role}
+            <input
+              className="form-check-input"
+              type="radio"
+              name="role"
+              id={`role-${i}`}
+              value={role}
+              onClick={() => this.setState({ role })}
+            />
+            <span className="circle">
+              <span className="check" />
+            </span>
           </label>
         </div>
       );
@@ -47,16 +51,19 @@ class Form extends React.Component {
     return industries.map((industry, i) => {
       return (
         <div key={`industry-${i}`} className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="industry"
-            id={`industry-${i}`}
-            value={industry}
-            onClick={() => this.setState({ industry })}
-          />
           <label className="form-check-label" htmlFor={`industry-${i}`}>
             {industry}
+            <input
+              className="form-check-input"
+              type="radio"
+              name="industry"
+              id={`industry-${i}`}
+              value={industry}
+              onClick={() => this.setState({ industry })}
+            />
+            <span className="circle">
+              <span className="check" />
+            </span>
           </label>
         </div>
       );
@@ -68,16 +75,19 @@ class Form extends React.Component {
     return frontEndFrameworks.map((frontEndFramework, i) => {
       return (
         <div key={`frontEndFramework-${i}`} className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="frontEndFramework"
-            id={`frontEndFramework-${i}`}
-            value={frontEndFramework}
-            onClick={() => this.setState({ frontEndFramework })}
-          />
           <label className="form-check-label" htmlFor={`frontEndFramework-${i}`}>
             {frontEndFramework}
+            <input
+              className="form-check-input"
+              type="radio"
+              name="frontEndFramework"
+              id={`frontEndFramework-${i}`}
+              value={frontEndFramework}
+              onClick={() => this.setState({ frontEndFramework })}
+            />
+            <span className="circle">
+              <span className="check" />
+            </span>
           </label>
         </div>
       );
@@ -89,16 +99,19 @@ class Form extends React.Component {
     return databases.map((database, i) => {
       return (
         <div key={`database-${i}`} className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="database"
-            id={`database-${i}`}
-            value={database}
-            onClick={() => this.setState({ database })}
-          />
           <label className="form-check-label" htmlFor={`database-${i}`}>
             {database}
+            <input
+              className="form-check-input"
+              type="radio"
+              name="database"
+              id={`database-${i}`}
+              value={database}
+              onClick={() => this.setState({ database })}
+            />
+            <span className="circle">
+              <span className="check" />
+            </span>
           </label>
         </div>
       );
@@ -108,24 +121,73 @@ class Form extends React.Component {
   // eslint-disable-next-line class-methods-use-this
   handleSubmit(event) {
     event.preventDefault();
+    const {
+      company,
+      greeting,
+      addresseeName,
+      role,
+      industry,
+      frontEndFramework,
+      database
+    } = this.state;
+    let missing = '';
+    if (company === '') {
+      missing = 'Company';
+    } else if (greeting === '') {
+      missing = 'Greeting';
+    } else if (greeting === 'custom' && addresseeName === '') {
+      missing = 'Addressee name';
+    } else if (role === '') {
+      missing = 'Role';
+    } else if (industry === '') {
+      missing = 'Industry';
+    } else if (frontEndFramework === '') {
+      missing = 'Front-End Framework';
+    } else if (database === '') {
+      missing = 'Database';
+    }
+    if (missing !== '') {
+      this.setState({ missing });
+      return;
+    }
+    let fixedGreeting = greeting;
+    if (greeting === 'team') {
+      fixedGreeting = `${company}'s  Team`;
+    }
+    this.props.generateLetter({
+      company,
+      fixedGreeting,
+      addresseeName,
+      role,
+      industry,
+      frontEndFramework,
+      database
+    });
   }
 
   render() {
     const { company } = this.state;
     return (
       <div>
+        {this.state.missing === '' ? (
+          ''
+        ) : (
+          <div className="alert alert-danger" role="alert">
+            Please choose your {this.state.missing}
+          </div>
+        )}
+
         <form action="">
           {/*  Company's name */}
           <div className="form-group row">
-            <label htmlFor="companyName" className="col-sm-2 col-form-label">
+            <label htmlFor="companyName" className="col-sm-2 bmd-label-floating">
               Company Name
             </label>
-            <div className="col-sm-10">
+            <div className="col-sm-2">
               <input
                 type="text"
                 className="form-control"
                 id="companyName"
-                placeholder="Company Name"
                 onChange={e => this.setState({ company: e.target.value })}
               />
             </div>
@@ -138,45 +200,55 @@ class Form extends React.Component {
               {/*  Hiring Manager greeting */}
               <div className="col-sm-10">
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="greeting"
-                    id="hiringManager"
-                    value="Hiring Manager"
-                    onClick={() => this.setState({ greeting: 'Hiring Manager' })}
-                  />
                   <label className="form-check-label" htmlFor="hiringManager">
                     Hiring Manager
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="greeting"
+                      id="hiringManager"
+                      value="Hiring Manager"
+                      onClick={() => this.setState({ greeting: 'Hiring Manager' })}
+                    />
+                    <span className="circle">
+                      <span className="check" />
+                    </span>
                   </label>
                 </div>
                 {/*  Company's team greeting */}
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="greeting"
-                    id="companyTeam"
-                    value="team"
-                    onClick={() => this.setState({ greeting: 'team' })}
-                  />
                   <label className="form-check-label" htmlFor="companyTeam">
                     {company === '' ? '[Company]' : company}
                     's Team
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="greeting"
+                      id="companyTeam"
+                      value="team"
+                      onClick={() => this.setState({ greeting: 'team' })}
+                    />
+                    <span className="circle">
+                      <span className="check" />
+                    </span>
                   </label>
                 </div>
                 {/*  Custom name for greeting */}
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="greeting"
-                    id="customName"
-                    value="custom"
-                    onClick={() => this.setState({ greeting: 'custom' })}
-                  />
                   <label className="form-check-label" htmlFor="customName">
                     <input
+                      className="form-check-input"
+                      type="radio"
+                      name="greeting"
+                      id="customName"
+                      value="custom"
+                      onClick={() => this.setState({ greeting: 'custom' })}
+                    />
+                    <span className="circle">
+                      <span className="check" />
+                    </span>
+                    <input
+                      className="form-control"
                       type="text"
                       placeholder="Addressee's Name"
                       onChange={e => this.setState({ addresseeName: e.target.value })}
@@ -203,29 +275,35 @@ class Form extends React.Component {
               {/*  Role appends */}
               <div className="col-sm-2">
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="role-append"
-                    id="developer"
-                    value="developer"
-                    onClick={() => this.setState({ roleAppend: 'Developer' })}
-                  />
                   <label className="form-check-label" htmlFor="developer">
                     Developer
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="role-append"
+                      id="developer"
+                      value="developer"
+                      onClick={() => this.setState({ roleAppend: 'Developer' })}
+                    />
+                    <span className="circle">
+                      <span className="check" />
+                    </span>
                   </label>
                 </div>
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="role-append"
-                    id="engineer"
-                    value="engineer"
-                    onClick={() => this.setState({ roleAppend: 'Engineer' })}
-                  />
                   <label className="form-check-label" htmlFor="engineer">
                     Engineer
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="role-append"
+                      id="engineer"
+                      value="engineer"
+                      onClick={() => this.setState({ roleAppend: 'Engineer' })}
+                    />
+                    <span className="circle">
+                      <span className="check" />
+                    </span>
                   </label>
                 </div>
               </div>
