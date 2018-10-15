8a70def311a8ba560008592edd88f3d3ed0ec43d
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-10-15 11:39:37 -0700

    Add API request and alert message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,57 @@
 import React from 'react';
 import Form from './Form';
+import Letter from './Letter';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      letter: '',
+      error: ''
+    };
+    this.generateLetter = this.generateLetter.bind(this);
+    this.errorMessage = this.errorMessage.bind(this);
+  }
+
+  generateLetter(data) {
+    this.setState({ error: '' });
+    fetch('/api/generate', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(raw => raw.json())
+      .then(({ letter }) => {
+        this.setState({ letter });
+      });
+  }
+
+  errorMessage(error) {
+    this.setState({ error }, () => {
+      window.scrollTo(0, 0);
+    });
   }
 
   render() {
     return (
       <div>
-        <Form />
+        {this.state.error === '' ? (
+          ''
+        ) : (
+          <div className="alert alert-danger" role="alert">
+            {this.state.error}
+          </div>
+        )}
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Form generateLetter={this.generateLetter} errorMessage={this.errorMessage} />
+            </div>
+            <div className="col">
+              <Letter letter={this.state.letter} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
