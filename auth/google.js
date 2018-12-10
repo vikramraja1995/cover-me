609f7fdffb8d19ebff56e8fcd476adb0c35ac7a1
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-12-10 10:46:01 -0800

    Add user registration

--- auth/google.js
+++ auth/google.js
@@ -10,7 +10,37 @@ passport.use(
       callbackURL: 'https://coverme.vikramraja.me/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOrCreate({ googleId: profile.id }, (err, user) => done(err, user));
+      console.log(profile);
+      console.log(JSON.stringify(profile));
+      // User.findOrCreate({ googleId: profile.id }, (err, user) => done(err, user));
+      User.findOne({ email: profile.email }, (err, user) => {
+        if (err) {
+          done(err);
+          return;
+        }
+        if (user) {
+          // If user with email exists but hasn't connected their google account
+          if (user.googleId !== profile.id) {
+            User.update(user, { $set: { googleId: profile.id } })
+              .then((savedUser) => {
+                done(null, savedUser);
+              })
+              .catch(err2 => console.err(err2));
+          }
+          done(null, user);
+          return;
+        }
+        // Create user if doesn't exist
+        const newUser = new User();
+        newUser.googleId = profile.id;
+        newUser.email = profile.email;
+        newUser
+          .save()
+          .then((savedUser) => {
+            done(null, savedUser);
+          })
+          .catch(err2 => console.err(err2)); // eslint-disable-line no-console
+      });
     },
   ),
 );
