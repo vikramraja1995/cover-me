542bfa183afc653e8cf07a2ca14cbca66ec84ff3
Vikram Raja <vikramraja1995@users.noreply.github.com>
2019-02-15 16:52:22 -0800

    Add/update db querying functions to be compatible with the new structure

--- database/mongodb.js
+++ database/mongodb.js
@@ -14,9 +14,21 @@ db.on('error', e => console.error('Connection Error:', e)); // eslint-disable-li
 /* --------------------------------------------------------------------------------------------- */
 
 // Set up Schemas
+const optionsFormat = { type: Map, of: String };
 
 const coverLetterSchema = new mongoose.Schema({
-  letter: String,
+  templateId: String,
+  savedOptions: optionsFormat,
+  dateCreated: Date,
+  dateModified: Date,
+});
+
+const templateSchema = new mongoose.Schema({
+  name: String,
+  template: String,
+  availableOptions: optionsFormat,
+  dateCreated: Date,
+  dateModified: Date,
 });
 
 const userSchema = new mongoose.Schema(
@@ -25,15 +37,12 @@ const userSchema = new mongoose.Schema(
     username: String,
     email: String,
     password: String,
-    coverLetters: [mongoose.Schema.Types.Mixed],
+    coverLetters: [coverLetterSchema],
+    templates: [templateSchema],
   },
   { strict: false },
 );
 
-const templateSchema = new mongoose.Schema({
-  name: String,
-  template: String,
-});
 /* --------------------------------------------------------------------------------------------- */
 
 // Set up password hashing and comparing functions
@@ -49,17 +58,31 @@ userSchema.methods.validatePassword = (password, hash, callback) => {
 // Set up Models
 const User = mongoose.model('user', userSchema);
 const Template = mongoose.model('template', templateSchema);
+const CoverLetter = mongoose.model('coverLetter', coverLetterSchema);
 /* --------------------------------------------------------------------------------------------- */
 
 // DB Querying Functions
-const addCoverLetter = (username, coverLetter) => User.findOne({ username }).then((doc) => {
-  doc.coverLetters.push(coverLetter);
-});
+// Add a generated cover letter options to user's history
+const addCoverLetter = (username, data) => {
+  const coverLetter = new CoverLetter(data);
+  const now = Date.now();
+  coverLetter.dateCreated = now;
+  coverLetter.dateModified = now;
+  return User.findOneAndUpdate({ username }, { $push: { coverLetter } });
+};
 
-const getCoverLetter = (username, coverLetter) => User.findOne({ 'coverLetters.name': coverLetter }).where({ username });
+// TODO: getCoverLetter() should get the saved cover letter options,
+// the template, fill out the information, and return all the data.
 
+// Get the options of a previously generated cover letter, and it's template
+const getCoverLetter = (username, coverLetter) => {
+  User.findOne({ 'coverLetters.name': coverLetter }).where({ username });
+};
+
+// Get a list of cover letters based on the company name
 const getCoverLetterList = username => User.find({ username }, 'coverLetters.name');
 
+// TODO: Fix this function
 const addUser = (name, username) => {
   const user = new User({
     name,
@@ -68,11 +91,29 @@ const addUser = (name, username) => {
   return user.save();
 };
 
-const addFieldToUser = (username, field, value) => {};
+// Get a list of available templates for a user
+const getTemplateList = username => User.findOne({ username }).then((res) => {
+  const templates = res.templates.map(template => ({
+    name: template.name,
+    templateId: template.id,
+  }));
+  return templates;
+});
 
-const removeFieldFromUser = (username, field, value) => {};
+// Get available options for a given template belonging to a user
+const getTemplateOptions = (username, templateId) => User.findOne({ username })
+  .then(user => user.templates.findById(templateId))
+  .then(template => template.availableOptions);
 
-const getTemplate = () => Template.findOne({});
+// Add a new template to the user's document
+const addTemplate = (username, data) => {
+  const template = new Template(data);
+  const now = Date.now();
+  template.dateCreated = now;
+  template.dateModified = now;
+  return User.findOneAndUpdate({ username }, { $push: { template } });
+};
+
 /* --------------------------------------------------------------------------------------------- */
 
 // Exports
@@ -81,7 +122,9 @@ module.exports = {
   getCoverLetterList,
   getCoverLetter,
   addUser,
-  getTemplate,
+  getTemplateList,
+  getTemplateOptions,
+  addTemplate,
   User,
   db,
 };
